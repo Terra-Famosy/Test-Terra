a0285ff188f10ee39a2e3dd6d5787aaba1cd8a61
Terra-Famosy <StellaHenkel.online@gmail.com>
2026-03-25 20:00:04 +0100

    Optimization: Display blocks only in the visible area

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -5,6 +5,7 @@
 #include <assetManager.h>
 #include <gameMap.h>
 #include <helpers.h>
+#include <raymath.h>
 
 struct GameData
 {
@@ -19,7 +20,7 @@ bool initGame()
 {
 	assetManager.loadAll();
 
-	gameData.gameMap.create(30, 30);
+	gameData.gameMap.create(700, 200);
 
 	for (int y = 0; y < gameData.gameMap.h; y++)
 		for (int x = 0; x < gameData.gameMap.w; x++)
@@ -83,12 +84,26 @@ bool updateGame()
 		}
 	}
 
-#pragma region drawWorld
+#pragma region draw world
 
 	BeginMode2D(gameData.camrea);
 
-	for (int y = 0; y < gameData.gameMap.h; y++)
-		for (int x = 0; x < gameData.gameMap.w; x++)
+	Vector2 topLeftView = GetScreenToWorld2D({ 0, 0 }, gameData.camrea);
+	Vector2 bottomRightView = GetScreenToWorld2D({ (float)GetScreenWidth(), (float)GetScreenHeight()}, gameData.camrea);
+
+	int startXView = (int)floor(topLeftView.x - 1);
+	int endXView = (int)floor(bottomRightView.x + 1);
+	int startYView = (int)floor(topLeftView.y - 1);
+	int endYView = (int)floor(bottomRightView.y + 1);
+
+	startXView = Clamp(startXView, 0, gameData.gameMap.w - 1);
+	endXView = Clamp(endXView, 0, gameData.gameMap.w - 1);
+
+	startYView = Clamp(startYView, 0, gameData.gameMap.h - 1);
+	endYView = Clamp(endYView, 0, gameData.gameMap.h - 1);
+
+	for (int y = startYView; y < endYView; y++)
+		for (int x = startXView; x < endXView; x++)
 		{
 			auto& b = gameData.gameMap.getBlockUnsafe(x, y);
 
@@ -108,8 +123,7 @@ bool updateGame()
 
 		}
 
-#pragma endregion
-
+	// Draw select block
 	DrawTexturePro(
 		assetManager.frame,
 		{ 0.f ,0.f, (float)assetManager.frame.width, (float)assetManager.frame.height }, // source
@@ -119,8 +133,11 @@ bool updateGame()
 		WHITE // tint
 	);
 
+#pragma endregion
+
 	EndMode2D();
 
+	DrawFPS(10, 10);
 
 	return !IsKeyDown(KEY_ESCAPE);
 }
